88e76463af75a6351e3b0a078eb5630852af9e41
CWestICL <cwest@ic.ac.uk>
2022-08-31 16:50:52 +0100

    new routes

--- routes/items.js
+++ routes/items.js
@@ -49,6 +49,56 @@ itemsRt.put('/:item/:user', async (req, res) => {
     const user = await User.findByPk(req.params.user);
     await item.addUser(user);
     res.send(200);
-})
+});
+
+itemsRt.delete('/:item', async (req, res) => {
+    const item = await Item.findByPk(req.params.item);
+    await item.destroy();
+    res.send(200);
+});
+
+itemsRt.put('/:item',[
+    check("title").trim().optional().not().isEmpty().withMessage('title must have content'),
+    check("price").optional().isNumeric().withMessage('price must be a number'),
+    check("description").trim().optional().not().isEmpty().withMessage('description must have content'),
+    check("category").trim().optional().not().isEmpty().withMessage('category must have content'),
+    check("image").optional().isURL().withMessage('image must be a URL')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log("Error: Invalid data submitted");
+        return res.status(400).send({error: errors.array()});
+    }
+    let findItem = await Show.findByPk(req.params.id);
+    let newItem = req.body;
+    if (newItem.title) {
+        await findItem.update({
+            title: newItem.title
+        });
+    }
+    if (newItem.price) {
+        await findItem.update({
+            price: newItem.price
+        });
+    }
+    if (newItem.description) {
+        await findItem.update({
+            description: newItem.description
+        });
+    }
+    if (newItem.category) {
+        await findItem.update({
+            category: newItem.category
+        });
+    }
+    if (newItem.image) {
+        await findItem.update({
+            image: newItem.image
+        });
+    }
+    console.log("Created new item: " + newItem.title);
+    let allItems = await Item.findAll();
+    res.status(202).send(allItems);
+});
 
 module.exports = {itemsRt}
