57a7e12262fce9a7f7d3bc283290b32289a3034b
CWestICL <cwest@ic.ac.uk>
2022-08-31 15:22:39 +0100

    post request server route

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Item = require('../models/Item')
+const {check,validationResult} = require("express-validator");
 const { items } = require('../seedData/seedData')
 // const {check, validationResult} = require('express-validator')
 const itemsRt = express.Router()
@@ -17,5 +18,30 @@ itemsRt.get('/:id', async (req, res) => {
     }
     res.send(data)
 })
+
+itemsRt.post('/',[
+    check("title").trim().not().isEmpty().withMessage('title must have content'),
+    check("price").isNumeric().withMessage('price must be a number'),
+    check("description").trim().not().isEmpty().withMessage('description must have content'),
+    check("category").trim().not().isEmpty().withMessage('category must have content'),
+    check("image").isURL().withMessage('image must be a URL')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log("Error: Invalid data submitted");
+        return res.status(400).send({error: errors.array()});
+    }
+    let newItem = req.body;
+    await Item.create({
+        title: newItem.title,
+        price: newItem.price,
+        description: newItem.description,
+        category: newItem.category,
+        image: newItem.image
+    });
+    console.log("Created new item: " + newItem.title);
+    let allItems = await Item.findAll();
+    res.status(202).send(allItems);
+});
 
 module.exports = {itemsRt}
